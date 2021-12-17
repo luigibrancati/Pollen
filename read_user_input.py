ee03c208823dd699edd876c774e8a70314ac84db
Luigi Brancati <luigi.brancati93@gmail.com>
2021-12-17 01:25:45 +0100

    Fixed typo

--- read_user_input.py
+++ read_user_input.py
@@ -2,8 +2,9 @@ from tkinter import Tk, ttk
 from pollen_classes import generate_bindings
 
 
-root = Tk()
-generate_bindings(root)
-button = ttk.Button(root, text="Quit", command=root.destroy)
-button.grid(column=3, row=7, padx=5, pady=5)
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    generate_bindings(root)
+    button = ttk.Button(root, text="Quit", command=root.destroy)
+    button.grid(column=3, row=7, padx=5, pady=5)
+    root.mainloop()
