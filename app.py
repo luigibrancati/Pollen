039ea3b35d382bacd9b9f212d8e5be0d3c3ec837
Luigi Brancati <luigi.brancati93@gmail.com>
2021-12-20 23:41:28 +0100

    Added windows to manage load and save, added reset button

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
-from tkinter import ttk, StringVar
+from tkinter import Toplevel, ttk, StringVar, Tk
 import pandas as pd
 import json
 
 
 # Pollen class is used both as a pollen representation and a tkinter frame
-class Pollini(ttk.Frame):
-    """Class to manage different pollen types."""
+class PollenFrame(ttk.Frame):
+    """Class to manage different pollen types.
+    This class contains data about the pollen family, name and count.
+    It also manages the frame to represent the pollen family
+    and its key binding.
+    """
 
     def __init__(self, master: ttk.Frame, fam: str, nome: str,
                  count: int = 0) -> None:
@@ -38,11 +42,17 @@ class Pollini(ttk.Frame):
     def add(self, event) -> None:
         """Callback function to increment pollen count."""
         self.tot += 1
-
-    def update_count(self, event) -> None:
-        """Callback function to update the label text."""
         self.contents.set(f"{self.famiglia}: {self.tot}")
-        self.label["textvariable"] = self.contents
+
+    def reset_count(self):
+        self.tot = 0
+        self.contents.set(f"{self.famiglia}: {self.tot}")
+
+    def set_binding(self, key: str) -> None:
+        # Bind the key to increase the pollen count
+        self.master.bind(f"<{key}>", self.add)
+        # Set the variable
+        self.key_bind.set(f"{key}")
 
     @property
     def __dict__(self) -> dict:
@@ -51,107 +61,155 @@ class Pollini(ttk.Frame):
                 "conteggio": self.tot}
 
     @classmethod
-    def generate_with_binding(cls: Pollini, master: ttk.Frame, fam: str,
-                              nome: str, key: str, count: int = 0) -> Pollini:
+    def generate_with_binding(cls: PollenFrame,
+                              master: ttk.Frame,
+                              fam: str,
+                              nome: str,
+                              key: str,
+                              count: int = 0) -> PollenFrame:
         """Class method used to generate a new pollen instance
         while at the same time binding it to a keyboard key.
         """
         # Instantiate pollen object
         pln = cls(master, fam, nome, count)
         # Bind the key to increase the pollen count
-        pln.master.bind(f"<{key}>", pln.add)
-        # Bind the key to also update the label value
-        pln.master.bind(f"<{key}>", pln.update_count, add='+')
-        pln.key_bind.set(f"{key}")
+        pln.set_binding(key)
         return pln
 
 
-class Application():
+class EntryFrame(Toplevel):
+    """This class opens a new window used to save/load data."""
+
+    def __init__(self, master: ttk.Frame | Tk, save: bool = True,
+                 *args, **kwargs) -> None:
+        super().__init__(master, *args, **kwargs)
+        self.data = None
+        self.master = master
+        self.entry = ttk.Entry(self, takefocus=True)
+        self.entry.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
+        # Cancel button
+        self.button_cancel = ttk.Button(self, text="Cancel",
+                                        command=self.destroy)
+        self.button_cancel.grid(row=1, column=2, padx=5, pady=5)
+
+        if save:
+            # Save button
+            self.function_button = ttk.Button(self, text="Save",
+                                              command=self._save)
+        else:
+            # Load button
+            self.function_button = ttk.Button(self, text="Load",
+                                              command=self._load)
+        self.function_button.grid(row=1, column=1, padx=5, pady=5)
+
+    def _save(self) -> None:
+        id = self.entry.get()
+        if id:
+            self.data.to_csv(f'./Vetrino_{id}.csv', sep=';', index=False)
+            self.destroy()
+        else:
+            print("Error, id not set.")
+
+    def _load(self) -> None:
+        bindings = ["Up", "Down", "Left", "Right", "a", "b",
+                    "c", "d", "e", "f", "g", "h", "i", "j",
+                    "k", "l", "m", "n", "o", "p", "q", "r",
+                    "s", "t", "u", "v"]
+        id = self.entry.get()
+        if id:
+            filename = f'./Vetrino_{id}.csv'
+            df = pd.read_csv(filename, sep=';', index_col=False)
+            vals = list(df.T.to_dict().values())
+            self.master.clear()
+            self.master.add_pollens(
+                [{**vals[i], 'key': bindings[i]} for i in range(len(vals))]
+            )
+            self.destroy()
+        else:
+            print("Error, id not set.")
+
+
+class Application(Tk):
     """Class to manage the pollen grid and placement,
     along with external buttons.
     """
-    def __init__(self: Application, master: ttk.Frame, cols: int) -> None:
-        self.master = master
+
+    def __init__(self, cols: int) -> None:
+        super().__init__()
+        self.title("Pollen Register")
         self.cols = cols
         self.pollen_frames = []
 
         # Load button
-        self.button_load = ttk.Button(self.master, text="Load",
-                                      command=self.load)
+        self.button_load = ttk.Button(self, text="Load",
+                                      command=self._load)
         # Save button
-        self.button_save = ttk.Button(self.master, text="Save",
-                                      command=self.save)
+        self.button_save = ttk.Button(self, text="Save",
+                                      command=self._save)
         # Quit button
-        self.button_quit = ttk.Button(self.master, text="Quit",
-                                      command=self.master.destroy)
-        self.id_entry = ttk.Entry(self.master)
-        self.id_entry.bind("<Return>", self.focus_on_master)
+        self.button_quit = ttk.Button(self, text="Quit",
+                                      command=self.destroy)
+        # Reset count button
+        self.button_reset_count = ttk.Button(self, text="Reset count",
+                                             command=self._reset_count)
 
-    def focus_on_master(self, event):
-        return self.master.focus()
-
-    def add_pollens(self, pollens: list[dict[str, str, str]]) -> None:
-        """Function to generate all bindings needed for the whole app."""
-        for pollen in pollens:
-            try:
-                pln = Pollini.generate_with_binding(self.master,
-                                                    pollen['famiglia'],
-                                                    pollen['nome'],
-                                                    pollen['key'],
-                                                    pollen['conteggio'])
-            except KeyError:
-                pln = Pollini.generate_with_binding(self.master,
-                                                    pollen['famiglia'],
-                                                    pollen['nome'],
-                                                    pollen['key'])
-            self.pollen_frames.append(pln)
-
-    def add_standard_pollens(self):
-        with open('./standard_pollen_mapping.json') as file:
-            self.add_pollens(json.load(file))
-
-    def draw_grid(self):
-        # Place pollens in frame
-        self.id_entry.grid(column=0, row=0, padx=5, pady=5)
-
+    def _draw_grid(self):
+        # Place pollens in frame master
         for i, pollen in enumerate(self.pollen_frames):
+            # Calculate the position of each pollen family
             col = i % self.cols
             row = i // self.cols + 1
             pollen.grid(column=col, row=row, padx=10, pady=10)
 
         n_plns = len(self.pollen_frames)
+        self.button_reset_count.grid(column=self.cols-4, row=n_plns+1,
+                                     padx=5, pady=5)
         self.button_load.grid(column=self.cols-3, row=n_plns+1, padx=5, pady=5)
         self.button_save.grid(column=self.cols-2, row=n_plns+1, padx=5, pady=5)
         self.button_quit.grid(column=self.cols-1, row=n_plns+1, padx=5, pady=5)
 
-    def save(self) -> None:
-        id = self.id_entry.get()
-        if id:
-            df = pd.DataFrame([vars(pln) for pln in self.pollen_frames])
-            df.to_csv(f'./Vetrino_{id}.csv', sep=';', index=False)
-        else:
-            print("Error, id not set.")
+    def _reset_count(self):
+        for p in self.pollen_frames:
+            p.reset_count()
 
-    def load(self) -> None:
-        bindings = ["Up", "Down", "Left", "Right", "a", "b",
-                    "c", "d", "e", "f", "g", "h", "i", "j",
-                    "k", "l", "m", "n", "o", "p", "q", "r",
-                    "s", "t", "u", "w"]
-        if id:
-            filename = f'./Vetrino_{self.id_entry.get()}.csv'
-            df = pd.read_csv(filename, sep=';', index_col=False)
-            self.pollen_frames = []
-            vals = list(df.T.to_dict().values())
-            self.add_pollens(
-                [{**vals[i], 'key': bindings[i]} for i in range(len(vals))]
-            )
-            self.draw_grid()
-        else:
-            print("Error, id not set.")
+    def clear(self):
+        self.pollen_frames = []
+
+    def add_pollens(self, pollens: list[dict[str, str, str]]) -> None:
+        """Function to generate all bindings needed for the whole app."""
+        for pollen in pollens:
+            try:
+                pln = PollenFrame.generate_with_binding(self,
+                                                        pollen['famiglia'],
+                                                        pollen['nome'],
+                                                        pollen['key'],
+                                                        pollen['conteggio'])
+            except KeyError:
+                pln = PollenFrame.generate_with_binding(self,
+                                                        pollen['famiglia'],
+                                                        pollen['nome'],
+                                                        pollen['key'])
+            self.pollen_frames.append(pln)
+        self._draw_grid()
+
+    def add_standard_pollens(self):
+        with open('./standard_pollen_mapping.json') as file:
+            self.add_pollens(json.load(file))
+        self._draw_grid()
+
+    def _save(self) -> None:
+        id_frame = EntryFrame(self)
+        id_frame.title("Save")
+        id_frame.data = pd.DataFrame([vars(pln) for pln in self.pollen_frames])
+        id_frame.mainloop()
+
+    def _load(self) -> None:
+        id_frame = EntryFrame(self, save=False)
+        id_frame.title("Load")
+        id_frame.mainloop()
 
     @classmethod
-    def generate_starting_frame(cls, master: ttk.Frame, cols: int = 5):
-        app = Application(master, cols)
+    def generate_starting_frame(cls, cols: int = 5) -> Application:
+        app = Application(cols)
         app.add_standard_pollens()
-        app.draw_grid()
         return app
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from tkinter import Tk
 from app import Application
 
 
 if __name__ == '__main__':
-    root = Tk()  # Main Frame object
-    Application.generate_starting_frame(root)  # Generate bindings
-    root.mainloop()  # Main event loop
+    app = Application.generate_starting_frame()  # Generate bindings
+    app.mainloop()  # Main event loop
