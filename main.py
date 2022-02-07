e07043302401ea22607331578dec3c393c5f01ca
Luigi Brancati <luigi.brancati93@gmail.com>
2022-02-08 00:22:01 +0100

    Moved logs to APPDATA

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import logging
 import os
 
 
-if not os.path.exists("./.logs"):
-    os.makedirs("./.logs")
-logging.basicConfig(filename=f"./.logs/log_{datetime.now().strftime('%Y%m%dT%H%M%S')}.log",
-                    level=logging.WARNING)
-
-
 if __name__ == '__main__':
+    app_data = os.getenv("LOCALAPPDATA")
+    if app_data is not None:
+        log_dir = f"{app_data}/Pollen/logs"
+    else:
+        log_dir = "./Pollen/logs"
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    logging.basicConfig(filename=f"{log_dir}/log_{datetime.now().strftime('%Y%m%dT%H%M%S')}.log",
+                        level=logging.WARNING)
+
     logging.debug("Starting app")
     app = Application.generate_starting_frame()  # Generate app
     app.mainloop()  # Main event loop
