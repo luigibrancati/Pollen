f83843f7b8cd813c5564ae3905416ace27f8b5f3
Luigi Brancati <luigi.brancati93@gmail.com>
2022-06-12 23:38:33 +0200

    translated help

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ _FONT_SIZE_MAIN = 10
 _FONT_SIZE_HELP = 11
 _FONT_SIZE_FOOTER = 8
 _GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScb56x4qFFO4iFOs8HHm59en611sn1XH-lrR9j5EbjgMj1T9w/viewform"
-_HELP_TEXT = "Each keyboard key is bound to a specific pollen family/name and is shown on the main window.\nOther keys:\n- Undo: {}\n- Redo: {}"
+_HELP_TEXT = "Ogni tasto è associato ad un polline, come mostrato nella finestra principale.\nPer modificare i tasti ed aggiungere/togliere pollini, modificare il file configuration.json.\n\nAltre chiavi:\n- Undo: {}\n- Redo: {}"
 _GOOGLE_FORM_TEXT = "Clicca qui per mandare un suggerimento o segnalare un bug"
