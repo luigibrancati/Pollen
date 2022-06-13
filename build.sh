76467d77dd40b3e75b408fa93670c35cf38bf801
Luigi Brancati <luigi.brancati93@gmail.com>
2022-06-13 17:14:19 +0200

    Added instructions file, update build script and ubuntu release

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-rm -rf dist/
-pyinstaller -c -w -i .\icons\pollen.ico --onefile -n Pollen --add-data configuration.json:configuration.json main.py
+pyinstaller -c -w -i .\icons\pollen.ico --onefile -n Pollen main.py
 chmod +x ./dist/Pollen
-cp configuration.json ./dist/
+mkdir temp
+cp ./dist/Pollen ./temp/
+cp configuration.json ./temp/
+cp LEGGIMI.txt ./temp/
+cd ./temp && tar -zcvf ../releases/Pollen_ubuntu_20-04.tar.gz . && cd -
+rm -rf ./temp
+rm -rf ./dist
+rm -rf ./build
